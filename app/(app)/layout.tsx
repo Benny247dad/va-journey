e17ff91bd49b22352ec5de8958019c0e97052c95
Bennkyking <122864114+Bennkyking@users.noreply.github.com>
2025-08-28 00:25:27 +0100

    removed Authprovider tag which appeared twice in the layout.tsx file

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,23 +1,17 @@
-// app/(app)/layout.tsx
-
 import { ReactNode } from "react";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-import AuthProvider from "@/context/AuthProvider";
-import ProtectedRoute from "@/components/ProtectedRoute";
 import AppTabBar from "@/components/AppTabBar";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <AuthProvider>
+    <>
       <Navbar />
-      <ProtectedRoute>
-        <main className="min-h-[calc(100vh-6rem)] pt-6 pb-20">
-          {children}
-        </main>
-        <AppTabBar />
-        <Footer /> {/* ✅ Moved the Footer inside the ProtectedRoute */}
-      </ProtectedRoute>
-    </AuthProvider>
+      <main className="min-h-[calc(100vh-6rem)] pt-6 pb-20">
+        {children}
+      </main>
+      <AppTabBar />
+      <Footer />
+    </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { Providers } from "./providers"; // ✅ Import the ThemeProvider wrapper
 import AuthProvider from "@/context/AuthProvider"; // ✅ Import the AuthProvider
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "VA Journey",
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Providers>
           <AuthProvider>
             {children}
+            <SpeedInsights />
           </AuthProvider>
         </Providers>
       </body>
